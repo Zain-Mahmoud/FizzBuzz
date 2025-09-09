ba72c080559ed463ce6b682f1cd9a5232c167f94
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2025-09-09 11:45:30 -0400

    made check() public

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -13,7 +13,7 @@ class FizzBuzz {
         }
     }
 
-    private static void check(int i) {
+    public static void check(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
